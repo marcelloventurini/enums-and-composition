598ebaccf4b121aad67a5e7d947cef82b2c822a5
Marcello Venturini <marcellofventurini@gmail.com>
2023-05-26 17:49:58 -0300

    create classes and enum

---
+++ EnumsAndComposition/Entities/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace EnumsAndComposition.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
